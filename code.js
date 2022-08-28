cb5e3b2e84282d56ec3d082059916d09c2816e70
austin <austinhuang1203@gmail.com>
2022-08-28 23:20:33 +0800

    added counter

--- code.js
+++ code.js
@@ -1,8 +1,17 @@
 
 // event listerners
-const btn = document.getElementById("button");
-btn.addEventListener("click",play_round);
+//const btn = document.getElementById("button");
+//btn.addEventListener("click",play_round);
+const btn1 = document.getElementById("button1");
+btn1.addEventListener("click",play_round1);
+const btn2 = document.getElementById("button2");
+btn2.addEventListener("click",play_round2);
+const btn3 = document.getElementById("button3");
+btn3.addEventListener("click",play_round3);
 
+// indexes
+let player_score = 0
+let computer_score = 0
 
 
 // rock = 7, paper = 8, scissors = 9
@@ -17,12 +26,70 @@ function playervcomputer (x){
     if (x == y){
        return 0;
     }
-    else if (x>y){
+    else if (x>y && (x-y)==1){
+        return 1;
+    }
+    else if (y>x && (y-x)==2){
         return 1;
     }
     else {
         return 2;
     }
+}
+
+function play_round1(){
+    let result = playervcomputer(7);
+
+    if (result == 0){
+        document.getElementById("demo").innerHTML = "Draw!";
+    }
+    else if (result == 1){
+        document.getElementById("demo").innerHTML = "Player Wins Round!";
+        player_score += 1;
+        document.getElementById("pscore").innerHTML = `Player Score: ${player_score}`;
+    }
+    else if (result == 2){
+        document.getElementById("demo").innerHTML = "Computer Wins Round!";
+        computer_score += 1;
+        document.getElementById("cscore").innerHTML = `Computer Score: ${computer_score}`;
+    }
+    return;
+}
+function play_round2(){
+    let result = playervcomputer(8);
+
+    if (result == 0){
+        document.getElementById("demo").innerHTML = "Draw!";
+    }
+    else if (result == 1){
+        document.getElementById("demo").innerHTML = "Player Wins Round!";
+        player_score += 1;
+        document.getElementById("pscore").innerHTML = `Player Score: ${player_score}`;
+    }
+    else if (result == 2){
+        document.getElementById("demo").innerHTML = "Computer Wins Round!";
+        computer_score += 1;
+        document.getElementById("cscore").innerHTML = `Computer Score: ${computer_score}`;
+    }
+    return;
+}
+function play_round3(){
+    let result = playervcomputer(9);
+
+    if (result == 0){
+        document.getElementById("demo").innerHTML = "Draw!";
+    }
+    else if (result == 1){
+        document.getElementById("demo").innerHTML = "Player Wins Round!";
+        player_score += 1;
+        document.getElementById("pscore").innerHTML = `Player Score: ${player_score}`;
+    }
+    else if (result == 2){
+        document.getElementById("demo").innerHTML = "Computer Wins Round!";
+        computer_score += 1;
+        document.getElementById("cscore").innerHTML = `Computer Score: ${computer_score}`;
+    }
+    return;
 }
 
 function play_round(){
